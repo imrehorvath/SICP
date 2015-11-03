675fa3cf9138a04a70936546a25a8f51b8848417
Imre Horvath <imi.horvath@gmail.com>
2015-11-03 20:39:38 +0100

    Implement missing procedures

--- oscexp.scm
+++ oscexp.scm
@@ -1,3 +1,28 @@
+(define (c+ x y)
+  (let ((z (make-connector)))
+    (adder x y z)
+    z))
+
+(define (c- x y)
+  (let ((z (make-connector)))
+    (adder y z x)
+    z))
+
+(define (c* x y)
+  (let ((z (make-connector)))
+    (multiplier x y z)
+    z))
+
+(define (c/ x y)
+  (let ((z (make-connector)))
+    (multiplier y z x)
+    z))
+
+(define (cv x)
+  (let ((z (make-connector)))
+    (constant x z)
+    z))
+
 (define (c^2 x)
   (let ((z (make-connector)))
     (squarer x z)
@@ -8,6 +33,9 @@
     (squarer z x)
     z))
 
+;;
+;; L = 1/[(2 * PI * f)^2 * C]
+;;
 (define (tank-circuit-calculator x y)
   (c/ (cv 1)
       (c* (c^2 (c* (cv 6.28318530718)
